35c55f5a3e5292b3bf0e1a1c95e71563eba7d296
Janis Volberg <janis@volberg.de>
2023-05-01 18:44:47 +0200

    kommentiert

--- Assets/Scripts/FPSLimit.cs
+++ Assets/Scripts/FPSLimit.cs
@@ -1,9 +1,12 @@
+// Bibliotheken importieren
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Klasse anlegen (erbt von Klasse MonoBehavior)
 public class FPSLimit : MonoBehaviour
 {
+    // Methode erstellen zum Festlegen der maximalen Bildrate beim Start
     void Start()
     {
         Application.targetFrameRate = 60;
