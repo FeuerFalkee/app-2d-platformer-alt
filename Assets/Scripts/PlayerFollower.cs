46b2c1d727ccb487b707e94f5babdd2eb2cf321f
Janis Volberg <janis@volberg.de>
2023-05-01 19:33:10 +0200

    Kommentare hinzugefügt

--- Assets/Scripts/PlayerFollower.cs
+++ Assets/Scripts/PlayerFollower.cs
@@ -1,16 +1,25 @@
+// Bibliotheken importieren
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Klasse anlegen (erbt von Klasse MonoBehavior)
 public class PlayerFollower : MonoBehaviour
 {
+    // Referenziert die Figur, der die Kamera folgt
     public GameObject player;
+    // Faktor der "weichen Bewegung" (Glättung) der Kameraführung
     public float smoothTime = 0.3F;
+
+    // Bewegungsgeschwindigkeit der Kamera
     private Vector3 velocity = Vector3.zero;
 
+    // Funktion, die bei jedem Frame ausgeführt wird
     void Update()
     {
-        Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y + 1.5f, transform.position.z);
+        // Berechnung der Kameraposition
+        Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // "Weiche Bewegung" der Kamera ausführen
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
